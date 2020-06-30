33c0ea3284887c51d0a7adfc36086a4e1deb2cd5
Durga Verma <durga.verma@cyware.com>
2020-06-30 22:31:10 +0530

    Class Demo2 is added

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,10 +1,11 @@
 
-public class Demo1 {
+public class Demo2 {
 
 	public static void main(String[] args) 
 	{
+		
 		System.out.println("Hello");
-		System.out.println("My name is Durga Verma");
+		System.out.println("My name is AmarSinghTanwar");
 
 	}
 
